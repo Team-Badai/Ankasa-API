9ba48745e24e12c9dfbec7061465b7812bf8c8cc
Juan Tombeng <juantombeng03@gmail.com>
2022-03-06 00:53:30 +0700

    rename folder controllers

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const route = express.Router();
-const userController = require("../controller/users");
+const userController = require("../controllers/users");
 const authenticator = require("../middleware/authenticator");
 const validator = require("../middleware/validator");
 
