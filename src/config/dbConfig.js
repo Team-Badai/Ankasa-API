1bebdd5222bd55bb1ed3b045de4dbefeedc25cc0
Juan Tombeng <juantombeng03@gmail.com>
2022-03-06 17:51:52 +0700

    fixing .env error

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql2')
-require('dotenv').config()
 
 const connection = mysql.createConnection({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME
+    // host : '34.232.44.145',
+    // user : 'fazz3juan',
+    // password : 'juantoFazz1234#',
+    // database : 'fazz3juan_ankasa'
 })
+
+console.log(process.env.DB_PASSWORD)
 
 connection.connect((err) => {
     if (err) throw err;
