c2024d69ecb3a6c508c884ac653e24a1bc52302f
Juan Tombeng <juantombeng03@gmail.com>
2022-03-08 06:18:45 +0700

    create booking, list of bookings, list of boarding pass

---
+++ src/controllers/bookings.js
@@ -0,0 +1,126 @@
+const bcrypt = require('bcrypt')
+const { v4 : uuidv4 } = require('uuid')
+const createError = require('http-errors')
+const {response, generateToken} = require('../helper/common')
+const usersQuery = require('../models/users')
+const flightsQuery = require('../models/flights')
+const bookingsQuery = require('../models/bookings')
+const ticketsQuery = require('../models/tickets')
+const paymentQuery = require('../models/payments')
+
+const booking = async (req, res, next) => {
+    try {
+        const {
+                id_flights, total_seats, total_luggage, total_price,
+                class_type, passengerData
+        } = req.body
+        const {email, status} = req.decoded
+        if (status === 1) {
+            const id_bookings = uuidv4()
+            const id_payments = uuidv4()
+            const ticketsArr = []
+            const [user] = await usersQuery.getUserIdByEmail(email)
+            const [flight_class] = await flightsQuery.getFlightClass(class_type)
+            const seatGenerator = () => {
+                let seat = ''
+                const Rows = 40
+                const Column = 'ABCDE'
+                for (let i = 0; i < 1; i++) {
+                    const col = Column.charAt(Math.floor(Math.random() * 5))
+                    const row = Math.floor(Math.random() * Rows) + 1
+                    seat = `${row}${col}`
+                }
+                return seat
+            }
+            const bookingData = {
+                id : id_bookings,
+                id_users : user.id,
+                id_payments : id_payments,
+                total_seats : total_seats,
+                total_luggage : total_luggage,
+                total_price : total_price,
+                payment_status : 'waiting for payment',
+                expiration_status : 'no'
+            }
+            const paymentData = {
+                id : id_payments
+            }
+            const newPayment = await paymentQuery.createPayment(paymentData)
+            if (newPayment.affectedRows > 0) {
+                const newBooking = await bookingsQuery.booking(bookingData)
+                if (newBooking.affectedRows > 0) {
+                    for (let i = 0; i < passengerData.length; i++) {
+                        const ticketData = {
+                            id : uuidv4(),
+                            id_flights : id_flights,
+                            id_bookings : id_bookings,
+                            id_classes : flight_class.id,
+                            passenger_name : passengerData[i].passengerName,
+                            passenger_category : passengerData[i].passengerCategory,
+                            passenger_nationality : passengerData[i].passengerNationality,
+                            travel_insurance : passengerData[i].travelInsurance,
+                            seat_number : seatGenerator(),
+                            luggage : passengerData[i].luggage,
+                            price : passengerData[i].price,
+                            eticket_status : 'pending payment',
+                            expiration_status : 'no'
+                        }
+                        const result = await ticketsQuery.purchaseTicket(ticketData)
+                        if (result.affectedRows > 0) {
+                            ticketsArr.push(ticketData)
+                        }
+                    }
+                    const results = {
+                        booking : bookingData,
+                        tickets : ticketsArr
+                    }
+                    response(res, results, 200, `Booking ID : ${id_bookings} with total ${total_seats} passengers.`)
+                }
+            }
+        } else {
+            response(res, null, 403, `Please verify your account first.`)
+        }
+    } catch (error) {
+        console.log(error.message);
+        next({ status: 500, message: `${error.message}` });
+    }
+}
+
+const listBookings = async (req, res, next) => {
+    try {
+        const {email, status} = req.decoded
+        if (status === 1) {
+            const [user] = await usersQuery.getUserIdByEmail(email)
+            const bookingsResult = await bookingsQuery.listBookings(user.id)
+            response(res, bookingsResult, 200, `List of booking user ${user.id}`)
+        } else {
+            response(res, null, 403, `Please verify your account first`)
+        }
+    } catch (error) {
+        console.log(error.message);
+        next({ status: 500, message: `${error.message}` });
+    }
+}
+
+const bookingDetails = async (req, res, next) => {
+    try {
+        const {email, status} = req.decoded
+        const {id_bookings} = req.body
+        if (status === 1) {
+            const [user] = await usersQuery.getUserIdByEmail(email)
+            const listTickets = await bookingsQuery.bookingDetails(id_bookings, user.id)
+            response(res, listTickets, 200, `List Of Boarding Pass with booking id : ${id_bookings}`)
+        } else {
+            response(res, null, 403, `Please verify your account first`)
+        }
+    } catch (error) {
+        console.log(error.message);
+        next({ status: 500, message: `${error.message}` });
+    }
+}
+
+module.exports = {
+    booking,
+    listBookings,
+    bookingDetails
+}
---
+++ src/models/bookings.js
@@ -0,0 +1,91 @@
+const connection = require('../config/dbConfig')
+
+const booking = (data) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `INSERT INTO bookings SET ?`
+        connection.query(sql, data, (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const listBookings = (id_users) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT bookings.id AS id_bookings, bookings.payment_status, bookings.total_price, bookings.total_seats, bookings.payment_status, 
+        flights.departure_date, flights.departure_time, flights.flight_number, 
+        origins.city AS origin_city, destinations.city AS destination_city, aircrafts.airline_name 
+        FROM tickets INNER JOIN bookings ON tickets.id_bookings = bookings.id 
+        INNER JOIN flights ON tickets.id_flights = flights.id 
+        INNER JOIN origins ON flights.id_origins = origins.id 
+        INNER JOIN destinations ON flights.id_destinations = destinations.id 
+        INNER JOIN aircrafts ON flights.id_aircrafts = aircrafts.id 
+        WHERE bookings.id_users = ? GROUP BY bookings.id`
+        connection.query(sql, id_users, (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const bookingDetails = (id_bookings, id_users) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT flights.id AS flight_id, flights.flight_number, flights.gate, flights.departure_date, flights.departure_time, 
+        aircrafts.airline_name, origins.city AS origin_city, destinations.city AS destination_city, destinations.terminal, 
+        flight_classes.class_type, tickets.id AS ticket_id, tickets.seat_number, tickets.passenger_name, tickets.eticket_status 
+        FROM tickets INNER JOIN flights ON tickets.id_flights = flights.id 
+        INNER JOIN aircrafts ON flights.id_aircrafts = aircrafts.id 
+        INNER JOIN origins ON flights.id_origins = origins.id 
+        INNER JOIN destinations ON flights.id_destinations = destinations.id 
+        INNER JOIN flight_classes ON tickets.id_classes = flight_classes.id 
+        INNER JOIN bookings ON tickets.id_bookings = bookings.id 
+        WHERE bookings.id = ? AND bookings.id_users = ?`
+        connection.query(sql, [id_bookings, id_users], (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const bookingPaymentDetail = (id_bookings, id_users) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT * FROM bookings WHERE id = ? AND id_users = ?`
+        connection.query(sql, [id_bookings, id_users], (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const updatePaymentStatus = (id_bookings, id_payments) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `UPDATE bookings SET payment_status = 'paid' WHERE id = ? AND id_payments = ?`
+        connection.query(sql, [id_bookings, id_payments], (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+module.exports = {
+    booking,
+    listBookings,
+    bookingDetails,
+    bookingPaymentDetail,
+    updatePaymentStatus
+}
