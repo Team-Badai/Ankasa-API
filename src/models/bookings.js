f40359732a4095c34fa972f1293ea9b22e9554eb
Juan Tombeng <juantombeng03@gmail.com>
2022-03-08 15:07:53 +0700

    testing list booking query

--- src/models/bookings.js
+++ src/models/bookings.js
@@ -13,18 +13,44 @@ const booking = (data) => {
     })
 }
 
+// const listBookings = (id_users) => {
+//     return new Promise ((resolve, reject) => {
+//         const sql = `SELECT bookings.id AS id_bookings, bookings.payment_status, bookings.total_price, bookings.total_seats, bookings.payment_status, 
+//         flights.departure_date, flights.departure_time, flights.flight_number, 
+//         origins.city AS origin_city, destinations.city AS destination_city, aircrafts.airline_name 
+//         FROM tickets INNER JOIN bookings ON tickets.id_bookings = bookings.id 
+//         INNER JOIN flights ON tickets.id_flights = flights.id 
+//         INNER JOIN origins ON flights.id_origins = origins.id 
+//         INNER JOIN destinations ON flights.id_destinations = destinations.id 
+//         INNER JOIN aircrafts ON flights.id_aircrafts = aircrafts.id 
+//         WHERE bookings.id_users = ? GROUP BY bookings.id`
+//         connection.query(sql, id_users, (error, result) => {
+//             if (!error) {
+//                 resolve(result)
+//             } else {
+//                 reject(error)
+//             }
+//         })
+//     })
+// }
+
 const listBookings = (id_users) => {
     return new Promise ((resolve, reject) => {
-        const sql = `SELECT bookings.id AS id_bookings, bookings.payment_status, bookings.total_price, bookings.total_seats, bookings.payment_status, 
-        flights.departure_date, flights.departure_time, flights.flight_number, 
-        origins.city AS origin_city, destinations.city AS destination_city, aircrafts.airline_name 
-        FROM tickets INNER JOIN bookings ON tickets.id_bookings = bookings.id 
-        INNER JOIN flights ON tickets.id_flights = flights.id 
-        INNER JOIN origins ON flights.id_origins = origins.id 
-        INNER JOIN destinations ON flights.id_destinations = destinations.id 
-        INNER JOIN aircrafts ON flights.id_aircrafts = aircrafts.id 
-        WHERE bookings.id_users = ? GROUP BY bookings.id`
+        const sql = `SELECT * FROM bookings WHERE id_users = ?`
         connection.query(sql, id_users, (error, result) => {
+            if (!error) {
+                resolve(result)
+            } else {
+                reject(error)
+            }
+        })
+    })
+}
+
+const getFlightDetailByBookingId = (id_bookings) => {
+    return new Promise ((resolve, reject) => {
+        const sql = `SELECT id_flights FROM tickets WHERE id_bookings = ?`
+        connection.query(sql, id_bookings, (error, result) => {
             if (!error) {
                 resolve(result)
             } else {
@@ -85,6 +111,7 @@ const updatePaymentStatus = (id_bookings, id_payments) => {
 module.exports = {
     booking,
     listBookings,
+    getFlightDetailByBookingId,
     bookingDetails,
     bookingPaymentDetail,
     updatePaymentStatus
