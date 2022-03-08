dddba6c9464210be8279f92c94bb6ffbbe521e7e
Juan Tombeng <juantombeng03@gmail.com>
2022-03-08 23:37:08 +0700

    change method search flight

--- src/routes/flights.js
+++ src/routes/flights.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const flightController = require('../controllers/flights')
 const {userTokenVerification} = require('../middleware/authenticator')
 
-route.get('/', flightController.getFlights)
+route.post('/', flightController.getFlights)
 route.get('/details', flightController.getFlightDetails)
 module.exports = route;
